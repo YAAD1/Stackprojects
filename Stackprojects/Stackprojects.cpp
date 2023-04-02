160c5e7d6d3eba00b26de0e052850e65c956d5a2
YAHYA DURSUN <yahyadursun@ogr.trakya.edu.tr>
2023-04-02 18:36:15 +0300

    Proje dosyası ekle.

---
+++ Stackprojects/Stackprojects.cpp
@@ -0,0 +1,11 @@
+﻿#define _CRT_SESCURE_NO_WARNINGS
+#include <iostream>
+#include <locale.h>
+#include <conio.h>
+#include<stdio.h>
+#include "c:\\C_MyLibrary\\StackProject.h"
+
+int main() {
+	menu();
+	
+}
